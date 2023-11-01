f71f2f7533cf0fecfb593627ba766a841522c661
Froillan Kim B. Edem <112741319+Froillan123@users.noreply.github.com>
2023-11-01 09:04:37 +0800

    Wala pani Function

--- js/script.js
+++ js/script.js
@@ -61,3 +61,28 @@ const typed = new Typed('.multiple-text',{
     loop: true
 });
 
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function() {
+    document.body.classList.toggle("light-mode"); 
+    if (document.body.classList.contains("light-mode")) {
+        icon.src = "images/sun.png";
+    } else {
+        icon.src = "images/moon.png"; 
+    }
+};
